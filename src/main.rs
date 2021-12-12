176cc24f1147e3e9f3908f22f2a638ff1515a20d
trickypr <trickypr@icloud.com>
2021-12-12 12:27:59 +1100

    Proof of concept

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use orbtk::prelude::*;
+
+fn main() {
+    Application::new()
+        .window(|ctx| {
+            Window::new()
+                .title("Slider resize example")
+                .position((100.0, 100.0))
+                .size(420.0, 730.0)
+                .resizeable(true)
+                .child(Slider::new().val(80).build(ctx))
+                .build(ctx)
+        })
+        .run();
+}
