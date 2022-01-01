0c5a34fb098eb9aa8d0eab40d1c03ec3f847f0be
trickypr <trickypr@icloud.com>
2022-01-01 16:31:58 +1100

    🚨 Fix depriciated function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
                 .title("Slider resize example")
                 .position((100.0, 100.0))
                 .size(420.0, 730.0)
-                .resizeable(true)
+                .resizable(true)
                 .child(Slider::new().val(80).build(ctx))
                 .build(ctx)
         })
